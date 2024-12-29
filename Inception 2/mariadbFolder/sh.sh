bb36426c02fca7f27ab7dc035ddb8757606cb0e7
Mohammed-Maghri <ouedzoum69@gmail.com>
2024-12-29 20:51:45 +0100

    Pushing the changes made

--- Inception 2/mariadbFolder/sh.sh
+++ Inception 2/mariadbFolder/sh.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 service mariadb start
 
 
 mariadb -e "CREATE DATABASE $DATA_BASE_NAME" 
 mariadb -e "CREATE USER '$DATA_BASE_USER'@'%' IDENTIFIED BY '$DATA_BASE_PASS'"   
 mariadb -e "GRANT ALL PRIVILEGES ON $DATA_BASE_NAME.* TO '$DATA_BASE_USER'@'%'"
+#mariadb -e "FLUSH PRIVILEGES"
 service mariadb stop
 
-exec mariadb 
+#mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+#exec mariadb 
