54bf97db35d62b91767b618edb4e9a09f728b71c
Mohammed-Maghri <ouedzoum69@gmail.com>
2024-12-30 18:42:26 +0100

    Pushing the Changes made

--- Inception 2/mariadbFolder/sh.sh
+++ Inception 2/mariadbFolder/sh.sh
@@ -1,7 +1,8 @@
-!/bin/bash
+#!/bin/bash
+
+mv /tmp/wp-config.php /var/www
 
 service mariadb start
-
 
 mariadb -e "CREATE DATABASE $DATA_BASE_NAME" 
 mariadb -e "CREATE USER '$DATA_BASE_USER'@'%' IDENTIFIED BY '$DATA_BASE_PASS'"   
@@ -9,4 +10,5 @@ mariadb -e "GRANT ALL PRIVILEGES ON $DATA_BASE_NAME.* TO '$DATA_BASE_USER'@'%'"
 mariadb -e "FLUSH PRIVILEGES"
 
 service mariadb stop
+
 mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
---
+++ Inception 2/wordpressFolder/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh php-fpm7.4 -F
