ae6564c6f421c2d21224b040a75c0e1de44a2e4c
Mohammed-Maghri <ouedzoum69@gmail.com>
2025-01-07 11:39:44 +0100

    Pushing the changes made

--- srcs/requirements/bonus/ftp/ftp.sh
+++ srcs/requirements/bonus/ftp/ftp.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-echo "Entering !"
-mkdir -p /var/run/vsftpd/empty
-touch /var/run/vsftpd/empty/file
-echo "File Generated"
-adduser --quiet --disabled-password --gecos "" "maghri"
-chown maghri:maghri /var/run/vsftpd/empty
-chmod 755 /var/run/vsftpd/empty
-echo   "maghri:325006" | chpasswd
-touch /home/maghri/file
-
-echo "write_enable=YES
-pasv_enable=YES
-chroot_local_user=YES
-pasv_min_port=40000
-pasv_max_port=40100
-pam_service_name=ftp
-allow_writeable_chroot=YES
-" >> /etc/vsftpd.conf
+echo    "Entering !"
+echo    "File Generated"
+mkdir   -p /var/run/vsftpd/empty
+adduser --quiet --disabled-password --gecos "" "$VSFTPD_USER"
+echo    "$VSFTPD_USER:$VSFTPD_PASSWD" | chpasswd
+touch   /home/$VSFTPD_USER/file
+echo    "secure_chroot_dir=/home/$VSFTPD_USER" >> /etc/
 vsftpd
