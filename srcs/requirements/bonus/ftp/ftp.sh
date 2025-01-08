16bbede0db6478e0cd0519b1d2a966fbb6d4b39a
Mohammed-Maghri <ouedzoum69@gmail.com>
2025-01-08 22:27:33 +0100

    Pushing The Changes For the Last Image

--- srcs/requirements/bonus/ftp/ftp.sh
+++ srcs/requirements/bonus/ftp/ftp.sh
@@ -5,5 +5,5 @@ mkdir   -p /var/run/vsftpd/empty
 adduser --quiet --disabled-password --gecos "" "$VSFTPD_USER"
 echo    "$VSFTPD_USER:$VSFTPD_PASSWD" | chpasswd
 touch   /home/$VSFTPD_USER/file
-echo    "secure_chroot_dir=/home/$VSFTPD_USER" >> /etc/
+
 vsftpd
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -10,8 +10,9 @@ sed -i 's/CHPASS/'$CONF_DB_PASSWORD'/' wp-config.php
 sed -i 's/CHHOST/'$CONF_DB_HOST'/' wp-config.php
 
 cd /var/www && wp core install --url=localhost --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
-wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PASSWORD} --path=/var/www
+cd /var/www && wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PASSWORD} --path=/var/www --allow-root
 
 cd /var/www && sudo -u www-data wp plugin install redis-cache --activate
 cd /var/www && sudo -u www-data wp redis enable
+
 php-fpm7.4 -F
